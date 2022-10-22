0296c3da46f276d99a32c67d2053d68af2674d59
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-10-22 22:10:01 +0530

    made the p controller

--- src/task1/src/controller.py
+++ src/task1/src/controller.py
@@ -23,12 +23,27 @@ class Controller():
         # Declare a Twist message
         self.vel = Twist()
 
+        # variables for P controller
+        self.kp = 2
+        self.error_global = [0, 0, 0]
+        self.error_local = [0, 0]       # only needs [x, y]
+
+        # Initialze Node
+        rospy.init_node('controller')
+        
+        # Initialze Publisher and Subscriber
+
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
+        rospy.Subscriber('/odom', Odometry, self.odometryCb)
+
+        # Initialise variables that may be needed for the control loop
+        # For ex: x_d, y_d, theta_d (in **meters** and **radians**) for defining desired goal-pose.
+        # and also Kp values for the P Controller
+
         # For maintaining control loop rate.
         self.rate = rospy.Rate(100)
 
-        # variables for P controller
-        self.kp = 0
-        self.error = [0, 0, 0]
 
     def odometryCb(self, msg):
 
@@ -44,55 +59,50 @@ class Controller():
 
     def main(self):
 
-        # Initialze Node
-        rospy.init_node('controller')
-        
-        # Initialze Publisher and Subscriber
-
-        pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-
-        rospy.Subscriber('/odom', Odometry, self.odometryCb)
-
-        # Initialise variables that may be needed for the control loop
-        # For ex: x_d, y_d, theta_d (in **meters** and **radians**) for defining desired goal-pose.
-        # and also Kp values for the P Controller
-
         while not rospy.is_shutdown():
 
             # Find error (in x, y and theta) in global frame
             # the /odom topic is giving pose of the robot in global frame
             # the desired pose is declared above and defined by you in global frame
             # therefore calculate error in global frame
-            self.error[0] = self.goal_position[0] - self.hola_position[0]
-            self.error[1] = self.goal_position[1] - self.hola_position[1]
-            self.error[2] = self.goal_position[2] - self.hola_position[2]
+            self.error_global[0] = self.goal_position[0] - self.hola_position[0]
+            self.error_global[1] = self.goal_position[1] - self.hola_position[1]
+            self.error_global[2] = self.goal_position[2] - self.hola_position[2]
 
             # (Calculate error in body frame)
             # But for Controller outputs robot velocity in robot_body frame, 
             # i.e. velocity are define is in x, y of the robot frame, 
             # Notice: the direction of z axis says the same in global and body frame
             # therefore the errors will have have to be calculated in body frame.
+            w = self.error_global[2]
+            self.error_local[0] = self.error_global[0]*math.cos(w) + self.error_global[1]*math.sin(w)
+            self.error_local[1] = self.error_global[0]*math.sin(w) + self.error_global[1]*math.cos(w)
 
             # Finally implement a P controller 
             # to react to the error with velocities in x, y and theta.
+            vel_x = self.kp * self.error_local[0]
+            vel_y = self.kp * self.error_local[1]
+            vel_z = self.kp * self.error_global[2]
 
             # Safety Check
             # make sure the velocities are within a range.
             # for now since we are in a simulator and we are not dealing with actual physical limits on the system 
             # we may get away with skipping this step. But it will be very necessary in the long run.
 
-            self.vel.linear.x = 0 #vel_x
-            self.vel.linear.y = 0 #vel_y
-            self.vel.angular.z = 0 #vel_z
-            print(self.error)
+            self.vel.linear.x = vel_x
+            self.vel.linear.y = vel_y
+            self.vel.angular.z = vel_z
+            # print(self.error_global, self.error_local)
+            print(vel_x, vel_y, vel_z)
 
-            pub.publish(self.vel)
+            self.pub.publish(self.vel)
             self.rate.sleep()
 
 	
 
 if __name__ == "__main__":
-	try:
-		main()
-	except rospy.ROSInterruptException:
-		pass
+    try:
+        control = Controller()
+        control.main()
+    except rospy.ROSInterruptException:
+        pass
