e80efb62a64f19a7a60089f6e742241914f737ca
kashyap-21 <joshikashyap379@gmail.com>
2022-10-22 13:19:50 +0530

    Task_1: ongoing

--- src/task1/src/controller.py
+++ src/task1/src/controller.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python3
+!/usr/bin/env python3
 
 import rospy
 
 # publishing to /cmd_vel with msg type: Twist
+
 from geometry_msgs.msg import Twist
 # subscribing to /odom with msg type: Odometry
 from nav_msgs.msg import Odometry
@@ -18,26 +19,51 @@ hola_y = 0
 hola_theta = 0
 
 def odometryCb(msg):
-	global hola_x, hola_y, hola_theta
 
-	# Write your code to take the msg and update the three variables
+    global hola_x, hola_y, hola_theta
+    # Write your code to take the msg and update the three variables
+    x = msg.pose.pose.orientation.x
+    y = msg.pose.pose.orientation.y
+    z = msg.pose.pose.orientation.z
+    w = msg.pose.pose.orientation.w
+
+    hola_x = msg.pose.pose.position.x
+    hola_y = msg.pose.pose.position.y
+
+    
+
+    hola_theta = euler_from_quaternion([x,y,z,w])[2]
+
+
+	
 
 def main():
-	# Initialze Node
+
+    # Initialze Node
+    rospy.init_node('controller')
 	# We'll leave this for you to figure out the syntax for 
 	# initialising node named "controller"
 	
 	# Initialze Publisher and Subscriber
+
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
+    rospy.Subscriber('/odom', Odometry, odometryCb)
 	# We'll leave this for you to figure out the syntax for
 	# initialising publisher and subscriber of cmd_vel and odom respectively
 
-	# Declare a Twist message
-	vel = Twist()
-	# Initialise the required variables to 0
+    # Declare a Twist message
+    vel = Twist()
+    
+    # Initialise the required variables to 0
 	# <This is explained below>
 	
+    rate = rospy.Rate(100)
+
+
+
 	# For maintaining control loop rate.
-	rate = rospy.Rate(100)
+	
 
 	# Initialise variables that may be needed for the control loop
 	# For ex: x_d, y_d, theta_d (in **meters** and **radians**) for defining desired goal-pose.
@@ -48,36 +74,17 @@ def main():
 	# Control Loop goes here
 	#
 	#
-	while not rospy.is_shutdown():
+    while not rospy.is_shutdown():
 
-		# Find error (in x, y and theta) in global frame
-		# the /odom topic is giving pose of the robot in global frame
-		# the desired pose is declared above and defined by you in global frame
-		# therefore calculate error in global frame
+        vel.linear.x = 0 #vel_x
+        vel.linear.y = 0 #vel_y
+        vel.angular.z = 0 #vel_z
+        print(math.degrees(hola_theta), hola_y, hola_x)
 
-		# (Calculate error in body frame)
-		# But for Controller outputs robot velocity in robot_body frame, 
-		# i.e. velocity are define is in x, y of the robot frame, 
-		# Notice: the direction of z axis says the same in global and body frame
-		# therefore the errors will have have to be calculated in body frame.
-		# 
-		# This is probably the crux of Task 1, figure this out and rest should be fine.
+        pub.publish(vel)
+        rate.sleep()
 
-		# Finally implement a P controller 
-		# to react to the error with velocities in x, y and theta.
-
-		# Safety Check
-		# make sure the velocities are within a range.
-		# for now since we are in a simulator and we are not dealing with actual physical limits on the system 
-		# we may get away with skipping this step. But it will be very necessary in the long run.
-
-		vel.linear.x = vel_x
-		vel.linear.y = vel_y
-		vel.angular.z = vel_z
-
-		pub.publish(vel)
-		rate.sleep()
-
+	
 
 if __name__ == "__main__":
 	try:
