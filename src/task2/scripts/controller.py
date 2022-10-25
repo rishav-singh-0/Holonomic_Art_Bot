ec636170743b41c4cb79ad10c4eb4107277ce072
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-10-25 18:03:14 +0530

    changed to class

--- src/task2/scripts/controller.py
+++ src/task2/scripts/controller.py
@@ -40,125 +40,123 @@ import math		# If you find it useful
 
 from tf.transformations import euler_from_quaternion	# Convert angles
 
-################## GLOBAL VARIABLES ######################
+class Controller():
+	PI = 3.14
 
-PI = 3.14
+	def __init__(self):
+		################## GLOBAL VARIABLES ######################
 
-x_goals = []
-y_goals = []
-theta_goals = []
+		self.x_goals = []
+		self.y_goals = []
+		self.theta_goals = []
 
-right_wheel_pub = None
-left_wheel_pub = None
-front_wheel_pub = None
+		self.right_wheel_pub = None
+		self.left_wheel_pub = None
+		self.front_wheel_pub = None
 
+		#################### ROS Node ############################
 
-##################### FUNCTION DEFINITIONS #######################
+		rospy.init_node('controller_node')
 
-# NOTE :  You may define multiple helper functions here and use in your code
+		signal.signal(signal.SIGINT, self.signal_handler)
 
-def signal_handler(sig, frame):
-	  
-	# NOTE: This function is called when a program is terminated by "Ctr+C" i.e. SIGINT signal 	
-	print('Clean-up !')
-	cleanup()
-	sys.exit(0)
+		self.right_wheel_pub = rospy.Publisher('/right_wheel_force', Wrench, queue_size=10)
+		self.front_wheel_pub = rospy.Publisher('/front_wheel_force', Wrench, queue_size=10)
+		self.left_wheel_pub = rospy.Publisher('/left_wheel_force', Wrench, queue_size=10)
 
-def cleanup():
-	############ ADD YOUR CODE HERE ############
+		rospy.Subscriber('detected_aruco',Pose2D,self.aruco_feedback_Cb)
+		rospy.Subscriber('task2_goals',PoseArray,self.task2_goals_Cb)
+		
+		self.rate = rospy.Rate(100)
 
-	# INSTRUCTIONS & HELP : 
-	#	-> Not mandatory - but it is recommended to do some cleanup over here,
-	#	   to make sure that your logic and the robot model behaves predictably in the next run.
+	##################### FUNCTION DEFINITIONS #######################
 
-	############################################
-  
-  
-def task2_goals_Cb(msg):
-	global x_goals, y_goals, theta_goals
-	x_goals.clear()
-	y_goals.clear()
-	theta_goals.clear()
+	def signal_handler(self, sig, frame):
+		
+		# NOTE: This function is called when a program is terminated by "Ctr+C" i.e. SIGINT signal 	
+		print('Clean-up !')
+		self.cleanup()
+		sys.exit(0)
 
-	for waypoint_pose in msg.poses:
-		x_goals.append(waypoint_pose.position.x)
-		y_goals.append(waypoint_pose.position.y)
+	def cleanup(self):
+		############ ADD YOUR CODE HERE ############
 
-		orientation_q = waypoint_pose.orientation
-		orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
-		theta_goal = euler_from_quaternion (orientation_list)[2]
-		theta_goals.append(theta_goal)
+		# INSTRUCTIONS & HELP : 
+		#	-> Not mandatory - but it is recommended to do some cleanup over here,
+		#	   to make sure that your logic and the robot model behaves predictably in the next run.
 
-def aruco_feedback_Cb(msg):
-	############ ADD YOUR CODE HERE ############
-
-	# INSTRUCTIONS & HELP : 
-	#	-> Receive & store the feedback / coordinates found by aruco detection logic.
-	#	-> This feedback plays the same role as the 'Odometry' did in the previous task.
-
-	############################################
-
-
-def inverse_kinematics():
-	############ ADD YOUR CODE HERE ############
-
-	# INSTRUCTIONS & HELP : 
-	#	-> Use the target velocity you calculated for the robot in previous task, and
-	#	Process it further to find what proportions of that effort should be given to 3 individuals wheels !!
-	#	Publish the calculated efforts to actuate robot by applying force vectors on provided topics
-	############################################
-
-
-def main():
-
-	rospy.init_node('controller_node')
-
-	signal.signal(signal.SIGINT, signal_handler)
-
-	# NOTE: You are strictly NOT-ALLOWED to use "cmd_vel" or "odom" topics in this task
-	#	Use the below given topics to generate motion for the robot.
-	right_wheel_pub = rospy.Publisher('/right_wheel_force', Wrench, queue_size=10)
-	front_wheel_pub = rospy.Publisher('/front_wheel_force', Wrench, queue_size=10)
-	left_wheel_pub = rospy.Publisher('/left_wheel_force', Wrench, queue_size=10)
-
-	rospy.Subscriber('detected_aruco',Pose2D,aruco_feedback_Cb)
-	rospy.Subscriber('task2_goals',PoseArray,task2_goals_Cb)
+		############################################
+		pass
 	
-	rate = rospy.Rate(100)
+	def task2_goals_Cb(self, msg):
+		self.x_goals.clear()
+		self.y_goals.clear()
+		self.theta_goals.clear()
 
-	############ ADD YOUR CODE HERE ############
+		for waypoint_pose in msg.poses:
+			self.x_goals.append(waypoint_pose.position.x)
+			self.y_goals.append(waypoint_pose.position.y)
 
-	# INSTRUCTIONS & HELP : 
-	#	-> Make use of the logic you have developed in previous task to go-to-goal.
-	#	-> Extend your logic to handle the feedback that is in terms of pixels.
-	#	-> Tune your controller accordingly.
-	# 	-> In this task you have to further implement (Inverse Kinematics!)
-	#      find three omni-wheel velocities (v1, v2, v3) = left/right/center_wheel_force (assumption to simplify)
-	#      given velocity of the chassis (Vx, Vy, W)
-	#	   
+			orientation_q = waypoint_pose.orientation
+			orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+			theta_goal = euler_from_quaternion (orientation_list)[2]
+			self.theta_goals.append(theta_goal)
 
-		
-	while not rospy.is_shutdown():
-		
-		# Calculate Error from feedback
+	def aruco_feedback_Cb(self, msg):
+		############ ADD YOUR CODE HERE ############
 
-		# Change the frame by using Rotation Matrix (If you find it required)
+		# INSTRUCTIONS & HELP : 
+		#	-> Receive & store the feedback / coordinates found by aruco detection logic.
+		#	-> This feedback plays the same role as the 'Odometry' did in the previous task.
 
-		# Calculate the required velocity of bot for the next iteration(s)
-		
-		# Find the required force vectors for individual wheels from it.(Inverse Kinematics)
+		############################################
+		pass
 
-		# Apply appropriate force vectors
+	def inverse_kinematics(self):
+		############ ADD YOUR CODE HERE ############
 
-		# Modify the condition to Switch to Next goal (given position in pixels instead of meters)
+		# INSTRUCTIONS & HELP : 
+		#	-> Use the target velocity you calculated for the robot in previous task, and
+		#	Process it further to find what proportions of that effort should be given to 3 individuals wheels !!
+		#	Publish the calculated efforts to actuate robot by applying force vectors on provided topics
+		############################################
+		pass
 
-		rate.sleep()
+	def main(self):
 
-    ############################################
+		############ ADD YOUR CODE HERE ############
+
+		# INSTRUCTIONS & HELP : 
+		#	-> Make use of the logic you have developed in previous task to go-to-goal.
+		#	-> Extend your logic to handle the feedback that is in terms of pixels.
+		#	-> Tune your controller accordingly.
+		# 	-> In this task you have to further implement (Inverse Kinematics!)
+		#      find three omni-wheel velocities (v1, v2, v3) = left/right/center_wheel_force (assumption to simplify)
+		#      given velocity of the chassis (Vx, Vy, W)
+		#	   
+			
+		while not rospy.is_shutdown():
+			
+			# Calculate Error from feedback
+
+			# Change the frame by using Rotation Matrix (If you find it required)
+
+			# Calculate the required velocity of bot for the next iteration(s)
+			
+			# Find the required force vectors for individual wheels from it.(Inverse Kinematics)
+
+			# Apply appropriate force vectors
+
+			# Modify the condition to Switch to Next goal (given position in pixels instead of meters)
+
+			self.rate.sleep()
+
+		############################################
 
 if __name__ == "__main__":
 	try:
-		main()
+		control = Controller()
+		control.main()
 	except rospy.ROSInterruptException:
 		pass
 
