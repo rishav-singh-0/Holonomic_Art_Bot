93da9a6f4782abb2a3f79a5c552942c753617eed
Rishav Singh <rishav.zero@pm.me>
2023-01-23 17:45:45 +0530

    Update main.ino

--- src/task4/main/main.ino
+++ src/task4/main/main.ino
@@ -53,29 +53,13 @@ void loop() {
 
   // Setting the speed
 
-  stepper_front.setSpeed(f_front);
-  stepper_left.setSpeed(f_left);
-  stepper_right.setSpeed(f_right);
-
-  // Giving the speed to motors
-
-  stepper_front.runSpeed();
-  stepper_left.runSpeed();
-  stepper_right.runSpeed();
+  set_run(f_front, f_left, f_right);
 
   delay(4000);
 
   // Re-setting the speed
 
-  stepper_front.setSpeed(0);
-  stepper_left.setSpeed(0);
-  stepper_right.setSpeed(0);
-
-  // Giving the speed to motors
-
-  stepper_front.runSpeed();
-  stepper_left.runSpeed();
-  stepper_right.runSpeed();
+  set_run(0, 0, 0);
 
   delay(100);
 
@@ -90,29 +74,13 @@ void loop() {
 
   // Setting the speed
 
-  stepper_front.setSpeed(f_front);
-  stepper_left.setSpeed(f_left);
-  stepper_right.setSpeed(f_right);
-
-  // Giving the speed to motors
-
-  stepper_front.runSpeed();
-  stepper_left.runSpeed();
-  stepper_right.runSpeed();
+  set_run(f_front, f_left, f_right);
 
   delay(4000);
 
   // Re-setting the speed
 
-  stepper_front.setSpeed(0);
-  stepper_left.setSpeed(0);
-  stepper_right.setSpeed(0);
-
-  // Giving the speed to motors
-
-  stepper_front.runSpeed();
-  stepper_left.runSpeed();
-  stepper_right.runSpeed();
+  set_run(0, 0, 0);
 
   delay(100);
 
@@ -126,29 +94,13 @@ void loop() {
 
   // Setting the speed
 
-  stepper_front.setSpeed(f_front);
-  stepper_left.setSpeed(f_left);
-  stepper_right.setSpeed(f_right);
-
-  // Giving the speed to motors
-
-  stepper_front.runSpeed();
-  stepper_left.runSpeed();
-  stepper_right.runSpeed();
+  set_run(f_front, f_left, f_right);
 
   delay(4000);
 
   // Re-setting the speed
 
-  stepper_front.setSpeed(0);
-  stepper_left.setSpeed(0);
-  stepper_right.setSpeed(0);
-
-  // Giving the speed to motors
-
-  stepper_front.runSpeed();
-  stepper_left.runSpeed();
-  stepper_right.runSpeed();
+  set_run(0, 0, 0);
 
   delay(10000);
   
@@ -160,4 +112,23 @@ void get_f_wheels( float x, float y, float w){
   f_left = (-0.333 * x) + (0.577367 * y) + (-0.16667 * w);
   f_right = (-0.333 * x) + (-0.577367 * y) + (-0.16667 * w);
 
+}
+
+void set_run(float f_front, float f_left, float f_right){
+    
+  // Setting the speed
+
+  stepper_front.setSpeed(f_front);
+  stepper_left.setSpeed(f_left);
+  stepper_right.setSpeed(f_right);
+
+  // Giving the speed to motors
+  
+  stepper_front.runSpeed();
+  stepper_left.runSpeed();
+  stepper_right.runSpeed();
+}
+
+void reset_speed(){
+  set_run(0, 0, 0);
 }
