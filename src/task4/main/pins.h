1cdfea71262cd14f860fb015dbb40b7924c4ff71
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2023-01-25 00:18:12 +0530

    changed pins

--- src/task4/main/pins.h
+++ src/task4/main/pins.h
@@ -1,10 +1,12 @@
 // Defining direction and step pins for the stepper motor
 
-#define FRONT_WHEEL_DIR 6           //4A
-#define FRONT_WHEEL_STEP 7          //4B
+#define FRONT_WHEEL_DIR 7           //4B
+#define FRONT_WHEEL_STEP 5          //3A
 
-#define LEFT_WHEEL_DIR 8            //4C
+#define LEFT_WHEEL_DIR 11           //1A
 #define LEFT_WHEEL_STEP 9           //2B
 
-#define RIGHT_WHEEL_DIR 10          //2A
-#define RIGHT_WHEEL_STEP 11         //1A
+#define RIGHT_WHEEL_DIR 46          //5A
+#define RIGHT_WHEEL_STEP 13         //0B
+
+#define MICROSTEP_2 44              //5C
