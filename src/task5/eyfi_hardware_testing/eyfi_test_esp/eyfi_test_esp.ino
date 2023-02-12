5d7ba988aaec5d04c9840d68c9962d176a3288c3
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2023-02-13 01:46:49 +0530

    added eyfi wifi connection esting scripts

---
+++ src/task5/eyfi_hardware_testing/eyfi_test_esp/eyfi_test_esp.ino
@@ -0,0 +1,53 @@
+#include <WiFi.h>
+
+// WiFi credentials
+const char* ssid = "Dark_Demon";                   //Enter your wifi hotspot ssid
+const char* password =  "apna_use_kar";            //Enter your wifi hotspot password
+const uint16_t port = 8002;
+const char * host = "18:1d:ea:45:9a:8e";           //Enter the ip address of your laptop after connecting it to wifi hotspot
+
+
+
+char incomingPacket[80];
+WiFiClient client;
+
+String msg = "0";
+
+
+
+
+void setup(){
+   
+  Serial.begin(115200);                          //Serial to print data on Serial Monitor
+  Serial1.begin(115200,SERIAL_8N1,33,32);        //Serial to transfer data between ESP and AVR. The Serial connection is inbuilt.
+  
+  
+  //Connecting to wifi
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("...");
+  }
+ 
+  Serial.print("WiFi connected with IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void loop() {
+
+  if (!client.connect(host, port)) {
+    Serial.println("Connection to host failed");
+    delay(200);
+    return;
+  }
+
+  while(1){
+      msg = client.readStringUntil('\n');         //Read the message through the socket until new line char(\n)
+      client.print("Hello from ESP32!");          //Send an acknowledgement to host(laptop)
+      Serial.println(msg);                        //Print data on Serial monitor
+      Serial1.println(msg);                       //Send data to AVR
+      Serial1.flush();
+    }
+}
