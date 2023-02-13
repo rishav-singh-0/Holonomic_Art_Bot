f0117e003efe733edef0fa3261f426015ed84833
rishav <rishav@no-reply.com>
2023-02-13 11:35:23 +0530

    removed unwanted functions

--- src/task5/main/main.ino
+++ src/task5/main/main.ino
@@ -12,33 +12,11 @@ typedef struct{
   bool isRunning;
 } Wheel;
 
-typedef struct {
-  float local_x;
-  float local_y;
-  float global_x;
-  float globla_y;
-
-  // angular position will be same for both frame local and global frame
-  float w;
-
-  // max speed and acc
-  float maxSpeed;
-  float maxAccleration;
-} Velocity;
-
 // initializing velocity variables 
 
-Velocity vel = {
-  .local_x = 0,
-  .local_y = 0,
-  .global_x = 0,
-  .globla_y = 0,
-  .w = 0,
-  .maxSpeed = 600,
-  .maxAccleration = 100,
-};
-
 Wheel wheel[3] = {{0, 0}, {0, 0}, {0, 0}};
+maxSpeed = 600;
+maxAccleration = 100;
 
 // Creating the objects for controlling the motors
 AccelStepper stepper_front(AccelStepper::DRIVER, FRONT_WHEEL_STEP, FRONT_WHEEL_DIR); 
@@ -51,38 +29,38 @@ void setup() {
 
   Serial.begin(115200);
 
-  stepper_front.setMaxSpeed(vel.maxSpeed);
-  stepper_front.setAcceleration(vel.maxAccleration);
-  stepper_left.setMaxSpeed(vel.maxSpeed);
-  stepper_left.setAcceleration(vel.maxAccleration);
-  stepper_right.setMaxSpeed(vel.maxSpeed);
-  stepper_right.setAcceleration(vel.maxAccleration);
+  stepper_front.setMaxSpeed(maxSpeed);
+  stepper_front.setAcceleration(maxAccleration);
+  stepper_left.setMaxSpeed(maxSpeed);
+  stepper_left.setAcceleration(maxAccleration);
+  stepper_right.setMaxSpeed(maxSpeed);
+  stepper_right.setAcceleration(maxAccleration);
 
   stepper_front.move(670.);
   stepper_left.move(670.);
   stepper_right.move(670.);
 
-  get_f_wheels(1,1,1);
-
 }
 
 void loop() {
   
-  run_speed();
+  // Printing the given speeds
+  static int count = 1;
+  count--;
+  if(count==0){
+    count = 10000;
+    Serial.print ("   ");
+    Serial.print(stepper_front.speed());
+    Serial.print ("   ");
+    Serial.print(stepper_left.speed());
+    Serial.print ("   ");
+    Serial.print(stepper_right.speed());
+    Serial.print ("   ");
+    Serial.print ("\n");
+  }
 
-  // static int count = 0;
-  // count++;
-  // if(count>10000){
-  //   count = 0;
-  //   Serial.print ("   ");
-  //   Serial.print(stepper_front.speed());
-  //   Serial.print ("   ");
-  //   Serial.print(stepper_left.speed());
-  //   Serial.print ("   ");
-  //   Serial.print(stepper_right.speed());
-  //   Serial.print ("   ");
-  //   Serial.print ("\n");
-  // }
+  // running the motors
+  run_speed();
 
   // checking if bot is ready to move
   static bool ready = false;
@@ -91,15 +69,15 @@ void loop() {
   }
   
   // validating conditions before moving
-  if (!ready && !cond_f && !cond_l && !cond_r){
+  if (!ready && !wheel[FRONT].isRunning && !wheel[LEFT].isRunning && !wheel[RIGHT].isRunning){
     ready = true;
   }
 
   // setting new speed for each wheel
   set_speed(
-    wheel[FRONT].force % vel.maxSpeed,
-    wheel[LEFT].force % vel.maxSpeed,
-    wheel[RIGHT].force % vel.maxSpeed
+    wheel[FRONT].force % maxSpeed,
+    wheel[LEFT].force % maxSpeed,
+    wheel[RIGHT].force % maxSpeed
   );
 
 }
@@ -109,7 +87,6 @@ void set_speed(float f_front, float f_left, float f_right){
   Sets new speed
   */
     
-  // Setting the speed
   stepper_front.setSpeed(wheel[FRONT].force);
   stepper_left.setSpeed(wheel[LEFT].force);
   stepper_right.setSpeed(wheel[RIGHT].force);
@@ -118,19 +95,11 @@ void set_speed(float f_front, float f_left, float f_right){
 void run_speed(){
   /*
   Run the motors
-  Store if motor has moved
+  Stores true if the motor was stepped for each motor
   */
 
   wheel[FRONT].isRunning = stepper_front.runSpeed();
   wheel[LEFT].isRunning = stepper_left.runSpeed();
   wheel[RIGHT].isRunning = stepper_right.runSpeed();
-
-}
-
-void get_f_wheels( float x, float y, float w){
-
-  wheel[FRONT].force  = (0.66667 * x) + (0 * y) + (-0.16667 * w);
-  wheel[LEFT].force  = (-0.333 * x) + (0.577367 * y) + (-0.16667 * w);
-  wheel[RIGHT].force  = (-0.333 * x) + (-0.577367 * y) + (-0.16667 * w);
 
 }
