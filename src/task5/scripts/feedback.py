a9ab0f22fabd04441954ac2ed0546b7313a7dea2
rishav <rishav@no-reply.com>
2023-02-14 17:30:32 +0530

    subscribed to usb_cam

--- src/task5/scripts/feedback.py
+++ src/task5/scripts/feedback.py
@@ -47,7 +47,7 @@ class Feedback():
 		#################### ROS Node ############################
 
 		rospy.init_node('aruco_feedback_node')
-		rospy.Subscriber('overhead_cam/image_raw', Image, self.callback)
+		rospy.Subscriber('usb_cam/image_raw', Image, self.callback)
 		self.aruco_publisher = rospy.Publisher('detected_aruco', Pose2D, queue_size=10)
 
 	##################### FUNCTION DEFINITIONS #######################
@@ -90,6 +90,10 @@ class Feedback():
 		# marking the detected area
 		cv2.aruco.drawDetectedMarkers(self.current_frame, corners)
 
+		# Camera window
+		cv2.imshow("Camera Window", self.current_frame)
+		cv2.waitKey(10)  # adding delay
+
 		# taking the 1st detected aruco marker
 		try:
 			ar = corners[0][0]
@@ -108,9 +112,6 @@ class Feedback():
 		# print(x, y, theta)
 			
 		self.publish(x, y, theta)
-		
-		cv2.imshow("Camera Window", self.current_frame)
-		cv2.waitKey(10)
 
 		# adding delay
 		rospy.sleep(0.001)
