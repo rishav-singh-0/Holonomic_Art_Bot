228d0879621d32fb19979167535905dead434f09
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2023-03-04 08:17:09 +0530

    added contour, penStatus publishers

--- src/task5b/src/path_planner.py
+++ src/task5b/src/path_planner.py
@@ -18,9 +18,13 @@ Also performs the task of position controller.
 
 import rospy
 import numpy as np
+import cv2
 
-from geometry_msgs.msg import Pose2D		# Message type used for receiving feedback
+# from geometry_msgs.msg import Pose2D		# Message type used for receiving feedback
+from cv_basics.msg import aruco_data
 from geometry_msgs.msg import Twist         # velocity
+from std_msgs.msg import String             # x and y setpoints/pixel list
+from std_msgs.msg import Int32              # taskStatus
 
 import time
 import math		# If you find it useful
@@ -34,20 +38,21 @@ class PathPlanner():
     def __init__(self):
         ################## GLOBAL VARIABLES ######################
 
-        self.x_goals = np.array([])
-        self.y_goals = np.array([])
-        self.theta_goals = np.array([])
+        self.x_goals = []
+        self.y_goals = []
+        self.theta_goals = []
         # self.x_goals = np.array([250, 255, 260, 270, 270, 270])
         # self.y_goals = np.array([250, 250, 250, 255, 260, 265])
         # self.theta_goals = np.array([0, 0, 0, 0, 0, 0])
-        # self.x_goals = np.array([150, 150, 350, 250])
-        # self.y_goals = np.array([300, 150, 150, 250])
-        # self.theta_goals = np.array([3*PI/4, -3*PI/4, -PI/4, 0])
+        # self.x_goals = np.array([np.array([150, 150, 350, 250]), np.array([150, 150, 350, 250])])
+        # self.y_goals = np.array([np.array([300, 150, 150, 250]), np.array([300, 150, 150, 250])])
+        # self.theta_goals = np.array([np.array([3*PI/4, -3*PI/4, -PI/4, 0]), np.array([3*PI/4, -3*PI/4, -PI/4, 0])])
 
         # position as [x, y, theta]
         self.hola_position = [0, 0, 0]              # current position
         self.goal_position = [None, None, None]     # current goals
 
+        self.contour_index = 0				# For travercing the contours
         self.goal_index = 0					# For travercing the setpoints
         
         self.max_setpoints = 30            # max setpoints to be trasversed by bot
@@ -64,6 +69,11 @@ class PathPlanner():
         self.vel.linear.x = 0.0
         self.vel.linear.y = 0.0
         self.vel.angular.z = 0.0
+        
+        # publish variables
+        self.cData = String()
+        self.penData = Int32()
+        self.penData.data = 0       # brush is not drawing
 
         #################### ROS Node ############################
 
@@ -71,9 +81,13 @@ class PathPlanner():
 
         self.rate = rospy.Rate(200)
 
-        self.goal_publisher = rospy.Publisher('path_plan', Twist, queue_size=10)
+        self.goal_publisher = rospy.Publisher('/path_plan', Twist, queue_size=10)
+        
+        self.contourPub = rospy.Publisher('/contours', String, queue_size=10)
+        self.penPub = rospy.Publisher('/penStatus', Int32, queue_size=10)
 
-        rospy.Subscriber('detected_aruco', Pose2D, self.aruco_feedback_Cb)
+        # rospy.Subscriber('/endSignal',Int32, self.end_signal_Cb) #optional
+        rospy.Subscriber('/detected_aruco', aruco_data, self.aruco_feedback_Cb)
         
     ##################### FUNCTION DEFINITIONS #######################
 
@@ -83,8 +97,11 @@ class PathPlanner():
         self.hola_position[2] = msg.theta
         # print(self.hola_position)
     
+    def end_signal_Cb(self, msg):
+        print(msg.data)
+
     def is_ready(self):
-        condition = self.x_goals.any() == False or \
+        condition = self.x_goals == [] or \
                     self.hola_position[0] == None
         # print(self.x_goals,self.hola_position)
         return condition
@@ -99,15 +116,29 @@ class PathPlanner():
         condition = self.threshold_box()
         # print(self.hola_position)
         if(condition):
-            if(self.goal_index < len(self.x_goals)-1):
+
+            if(self.contour_index < len(self.x_goals)-1):
+                self.contour_index += 1
+                self.goal_index = 0
+                self.penData.data = 0
+                self.penPub.publish(self.penData)
+                return
+
+            if(self.goal_index < len(self.x_goals[self.contour_index])-1):
                 self.goal_index += 1
                 rospy.sleep(0.1)
                 rospy.loginfo("Goal reached " + str(self.goal_index)+": "+str(self.goal_position))
                 self.goal_position = [
-                    self.x_goals[self.goal_index], 
-                    self.y_goals[self.goal_index], 
-                    self.theta_goals[self.goal_index]
+                    self.x_goals[self.contour_index][self.goal_index], 
+                    self.y_goals[self.contour_index][self.goal_index], 
+                    self.theta_goals[self.contour_index][self.goal_index]
                 ]
+            if(self.goal_index == 1):
+                self.penData.data = 1
+            else:
+                self.penData.data = 1
+            self.penPub.publish(self.penData)
+            
 
     def safety_check(self):
         '''
@@ -143,21 +174,84 @@ class PathPlanner():
         self.safety_check()
         
     def image_mode(self):
-        self.x_goals = []
-        self.y_goals = []
-        self.theta_goals = []
+        
+        size_img = (500,500)
+        # max_points = 20
+
+        # img = cv2.imread('/mnt/STORAGE/project/hola_bot/src/task5b/src/snapchat.png',0)
+        img = cv2.imread('/mnt/STORAGE/project/hola_bot/src/task5b/src/smile.png',0)
+
+        img = cv2.resize(img,size_img)
+        black = np.zeros((size_img[0],size_img[1],3),np.uint8)
+
+        _, thresh = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+        contours,hierarchy = cv2.findContours(thresh, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+
+        xList, yList, wList = [], [], []
+
+        x_goals = []
+        y_goals = []
+        theta_goals = []
+
+        for i in range(0,len(contours)):
+
+            xList = []
+            yList = []
+            wList = []
+
+            len_cont = len(contours[i])
+
+            if(hierarchy[0][i][3] != -1):
+                for j in range(0,len_cont, 10):
+                    black[contours[i][j][0][1]][contours[i][j][0][0]] = 255
+                    xList.append(contours[i][j][0][0])
+                    yList.append(contours[i][j][0][1])
+                    wList.append(0)
+            
+                x_goals.append(xList)
+                y_goals.append(yList)
+                theta_goals.append(wList)
+        
+        # print(x_goals, len(x_goals))
+        # print(y_goals, len(y_goals))
+        # cv2.imwrite("out_def.png",black)
+
+        self.x_goals = x_goals
+        self.y_goals = y_goals
+        self.theta_goals = theta_goals
+
+        self.publish_contours()
 
     def function_mode(self):
         
         # take few points from 0 to 2*PI and generate setpoints in x, y and theta arrays
         t = np.linspace(0, 2*PI, num=self.max_setpoints)       
-        x = lambda t: 150*math.cos(t) + 250
-        y = lambda t: 80*math.sin(2*t) + 250
+        x = lambda t: 200*math.cos(t) + 250
+        y = lambda t: 100*math.sin(2*t) + 250
         theta = lambda t: (PI/4)*math.sin(t) # you may need to add a phase shift
         
-        self.x_goals = np.array([x(i) for i in t])
-        self.y_goals = np.array([y(i) for i in t])
-        self.theta_goals = np.array([theta(i) for i in t])
+        self.x_goals = np.array([np.array([x(i) for i in t])])
+        self.y_goals = np.array([np.array([y(i) for i in t])])
+        self.theta_goals = np.array([np.array([theta(i) for i in t])])
+
+        self.x_goals = list(self.x_goals)
+        self.y_goals = list(self.x_goals)
+        self.theta_goals = list(self.theta_goals)
+        self.publish_contours()
+
+    def publish_contours(self):
+        # publishing contour
+        self.cData.data = str([self.x_goals,self.y_goals])
+        self.contourPub.publish(self.cData)
+
+
+    def publisher(self):
+        self.goal_publisher.publish(self.vel)
+        # rospy.loginfo("Goal: "+str([self.vel.linear.x, self.vel.linear.y, self.vel.angular.z]))
+        
+        # self.penData.data = 1
+        # self.penPub.publish(self.penData)
+        self.publish_contours()
 
 
     def main(self):
@@ -174,9 +268,9 @@ class PathPlanner():
             # removing error while going to next test case
             try:
                 self.goal_position = [
-                    self.x_goals[self.goal_index], 
-                    self.y_goals[self.goal_index], 
-                    self.theta_goals[self.goal_index]
+                    self.x_goals[self.contour_index][self.goal_index], 
+                    self.y_goals[self.contour_index][self.goal_index], 
+                    self.theta_goals[self.contour_index][self.goal_index]
                 ]
             except IndexError as e:
                 rospy.logerr(e)
@@ -186,15 +280,15 @@ class PathPlanner():
             # print(self.hola_position)
 
             # print(self.vel)
-            self.goal_publisher.publish(self.vel)
-            # rospy.loginfo("Goal: "+str([self.vel.linear.x, self.vel.linear.y, self.vel.angular.z]))
+            self.publisher()
             self.next_goal()
 
 
 if __name__ == "__main__":
     try:
         control = PathPlanner()
-        control.function_mode()
+        # control.function_mode()
+        control.image_mode()
         control.main()
     except rospy.ROSInterruptException:
         pass
