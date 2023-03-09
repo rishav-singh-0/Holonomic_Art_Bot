45d43ed939051f4b745a23a3b3b1a2b6d088b450
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2023-03-09 08:46:01 +0530

    added file level doc for avrSection.ino

--- src/task5b/avrSection/avrSection.ino
+++ src/task5b/avrSection/avrSection.ino
@@ -1,4 +1,15 @@
+/*
+* Team Id: HB#1254
+* Author List: Rishav
+* Filename: avrSection.ino
+* Theme: Hola Bot -- Specific to eYRC 2022-23
+* Functions: setup(), loop(), set_speed(), run_speed()
+* Global Variables: 
+*    wheel, maxSpeed, maxAccleration, recData, stepper, myservo, servo_pos, up_pos, down_pos
+*/
+
 #include <AccelStepper.h>
+#include <Servo.h>
 #include "pins.h"
 
 // Task 5
@@ -17,12 +28,18 @@ typedef struct{
 Wheel wheel[3] = {{0, 0}, {0, 0}, {0, 0}};
 int maxSpeed = 600;
 int maxAccleration = 100;
-String rec_data = "0";
+String recData = "0";
 
 // Creating the objects for controlling the motors
-AccelStepper stepper_front(AccelStepper::DRIVER, FRONT_WHEEL_STEP, FRONT_WHEEL_DIR); 
-AccelStepper stepper_left(AccelStepper::DRIVER, LEFT_WHEEL_STEP, LEFT_WHEEL_DIR); 
-AccelStepper stepper_right(AccelStepper::DRIVER, RIGHT_WHEEL_STEP, RIGHT_WHEEL_DIR);
+AccelStepper stepper[3];
+AccelStepper stepper[FRONT](AccelStepper::DRIVER, FRONT_WHEEL_STEP, FRONT_WHEEL_DIR); 
+AccelStepper stepper[LEFT](AccelStepper::DRIVER, LEFT_WHEEL_STEP, LEFT_WHEEL_DIR); 
+AccelStepper stepper[RIGHT](AccelStepper::DRIVER, RIGHT_WHEEL_STEP, RIGHT_WHEEL_DIR);
+
+Servo myservo;        // create servo object to control a servo
+bool servo_pos = 0;
+int up_pos = 10;        // variable to store the servo position
+int down_pos = 95;        // variable to store the servo position
 
 
 void setup() {
@@ -32,16 +49,16 @@ void setup() {
   pinMode(GREEN,OUTPUT);
   pinMode(BLUE,OUTPUT);
 
-  stepper_front.setMaxSpeed(maxSpeed);
-  stepper_front.setAcceleration(maxAccleration);
-  stepper_left.setMaxSpeed(maxSpeed);
-  stepper_left.setAcceleration(maxAccleration);
-  stepper_right.setMaxSpeed(maxSpeed);
-  stepper_right.setAcceleration(maxAccleration);
+  stepper[FRONT].setMaxSpeed(maxSpeed);
+  stepper[FRONT].setAcceleration(maxAccleration);
+  stepper[LEFT].setMaxSpeed(maxSpeed);
+  stepper[LEFT].setAcceleration(maxAccleration);
+  stepper[RIGHT].setMaxSpeed(maxSpeed);
+  stepper[RIGHT].setAcceleration(maxAccleration);
 
-  stepper_front.move(0.);
-  stepper_left.move(0.);
-  stepper_right.move(0.);
+  stepper[FRONT].move(0.);
+  stepper[LEFT].move(0.);
+  stepper[RIGHT].move(0.);
 
   Serial.begin(115200);
 }
@@ -50,83 +67,85 @@ void loop() {
 
   static bool ready = false;
 
-  if(Serial.available()){                  //Check if any data is available on Serial
-    rec_data = Serial.readStringUntil('\n');    //Read message on Serial until new char(\n) which indicates end of message. Received data is stored in msg
+  //Check if any data is available on Serial
+  if(Serial.available()){                  
 
-    char *buf = rec_data.c_str();
-    // sscanf(buf, "{%f, %f, %f}\n", &wheel[FRONT].force, &wheel[LEFT].force, &wheel[RIGHT].force);
+    //Read message on Serial until new char(\n) which indicates end of message. Received data is stored in msg
+    recData = Serial.readStringUntil('\n');
+
+    char *buf = recData.c_str();
 
     wheel[FRONT].force = atof(strtok(buf+1, ","));
     wheel[LEFT].force = atof(strtok(NULL, ","));
     wheel[RIGHT].force = atof(strtok(NULL, ","));
+    servo_pos = atoi(strtok(NULL, ","));
 
-    // Serial.print(rec_data.c_str());
-
-    // Serial.print ("\n");
-    // Serial.print(wheel[FRONT].force);
-    // Serial.print("   ");
-    // Serial.print(wheel[LEFT].force);
-    // Serial.print("   ");
-    // Serial.print(wheel[RIGHT].force);
-    // Serial.print ("\n");
     ready = true;
   }
   else{
     ready = false;
   }
 
-  // Printing the given speeds
+  // Printing the given speeds for debugging
   static int count = 1;
   count--;
   if(count==0){
     count = 1000000;
     Serial.print("   ");
-    Serial.print(stepper_front.speed());
+    Serial.print(stepper[FRONT].speed());
     Serial.print("   ");
-    Serial.print(stepper_left.speed());
+    Serial.print(stepper[LEFT].speed());
     Serial.print("   ");
-    Serial.print(stepper_right.speed());
+    Serial.print(stepper[RIGHT].speed());
     Serial.print("   ");
+    Serial.print(servo_pos);
     Serial.print ("\n");
   }
 
-  // running the motors
-  run_speed();
+  if(servo_pos == 1){
+    myservo.write(down_pos);         // tell servo to go to position in variable 'down_pos'
+  }
+  else{
+    myservo.write(up_pos);           // tell servo to go to position in variable 'up_pos'
+  }
 
   // checking if bot is ready to move
   if (!ready) {
     return;
   }
   
-  // validating conditions before moving
-  // if (!wheel[FRONT].isRunning && !wheel[LEFT].isRunning && !wheel[RIGHT].isRunning){
-  //   ready = true;
-  // }
+  // running the motors
+  run_speed();
 
   // setting new speed for each wheel
   set_speed(wheel[FRONT].force, wheel[LEFT].force, wheel[RIGHT].force);
 
 }
-
-void set_speed(float f_front, float f_left, float f_right){
-  /*
-  Sets new speed
-  */
+ 
+/*
+* Function Name: set_speed
+* Logic: sets the new wheel speed to the AccelStepper objects of each wheel
+* Example Call: 
+*   set_speed()
+*/ 
+void set_speed(){
     
-  stepper_front.setSpeed(f_front);
-  stepper_left.setSpeed(f_left);
-  stepper_right.setSpeed(f_right);
+  stepper[FRONT].setSpeed(wheel[FRONT].force);
+  stepper[LEFT].setSpeed(wheel[LEFT].force);
+  stepper[RIGHT].setSpeed(wheel[RIGHT].force);
   
 }
 
+/*
+* Function Name: run_speed
+* Logic: Runs the motors and stores true if the motor was stepped for each motor
+* Example Call: 
+*   run_speed()
+*/ 
 void run_speed(){
-  /*
-  Run the motors
-  Stores true if the motor was stepped for each motor
-  */
 
-  wheel[FRONT].isRunning = stepper_front.runSpeed();
-  wheel[LEFT].isRunning = stepper_left.runSpeed();
-  wheel[RIGHT].isRunning = stepper_right.runSpeed();
+  wheel[FRONT].isRunning = stepper[FRONT].runSpeed();
+  wheel[LEFT].isRunning = stepper[LEFT].runSpeed();
+  wheel[RIGHT].isRunning = stepper[RIGHT].runSpeed();
 
 }
