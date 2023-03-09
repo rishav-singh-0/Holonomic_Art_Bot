41eabe2bd1b3015ed9b145698120dbd28561ed2f
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2023-03-09 08:17:02 +0530

    added file level doc for espSection.ino

--- src/task5b/espSection/espSection.ino
+++ src/task5b/espSection/espSection.ino
@@ -1,27 +1,35 @@
+/*
+* Team Id: HB#1254
+* Author List: Rishav
+* Filename: espSection.ino
+* Theme: Hola Bot -- Specific to eYRC 2022-23
+* Functions: setup(), loop()
+* Global Variables: 
+*    ssid, password, port, host, previousMillis, interval, client, receivedMsg
+*/
+
 #include <WiFi.h>
 
 #define BAUD_RATE 115200
 
 // WiFi credentials
-const char* ssid = "Dark_Demon";                   //Enter your wifi hotspot ssid
-const char* password =  "apna_use_kar";            //Enter your wifi hotspot password
+const char* ssid = "Dark_Demon";                   //WiFi hotspot ssid
+const char* password =  "apna_use_kar";            //WiFi hotspot password
 const uint16_t port = 8002;
-const char * host = "192.168.43.129";           //Enter the ip address of your laptop after connecting it to wifi hotspot
+const char * host = "192.168.43.129";             //The ip address of laptop(base station) after connecting it to wifi hotspot
 
 unsigned long previousMillis = 0;
 unsigned long interval = 8000;
 
-char incomingPacket[80];
 WiFiClient client;
 
-String msg = "0";
-
+String receivedMsg = "0";
 
 void setup(){
    
   Serial.begin(BAUD_RATE);                          //Serial to print data on Serial Monitor
   Serial.print("Hello Rishav!\n");
-  Serial1.begin(BAUD_RATE, SERIAL_8N1, 33, 32);        //Serial to transfer data between ESP and AVR. The Serial connection is inbuilt.
+  Serial1.begin(BAUD_RATE, SERIAL_8N1, 33, 32);     //Serial to transfer data between ESP and AVR. The Serial connection is inbuilt.
     
   //Connecting to wifi
   WiFi.begin(ssid, password);
@@ -54,10 +62,10 @@ void loop() {
   }
   
   while(1){
-      msg = client.readStringUntil('\n');         //Read the message through the socket until new line char(\n)
-      client.print("Hello from ESP32!");          //Send an acknowledgement to host(laptop)
-      Serial.println(msg);                        //Print data on Serial monitor
-      Serial1.println(msg);                       //Send data to AVR
+      receivedMsg = client.readStringUntil('\n');     //Read the message through the socket until new line char(\n)
+      client.print("Hello from ESP32!");              //Send an acknowledgement to host(laptop)
+      Serial.println(receivedMsg);                    //Print data on Serial monitor
+      Serial1.println(receivedMsg);                   //Send data to AVR
       Serial1.flush();
   }
 }
